90bae11064bc683e80fb1167c783037690d120c8
97lunarmare <matthewgl0@outlook.com>
2021-09-07 21:37:42 +0100

    commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Home from "./Home.svelte";
+</script>
+
+<main>
+	<Home/>
+</main>
+
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
